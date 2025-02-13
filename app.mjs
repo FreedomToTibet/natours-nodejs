0796cc1bdb3df744781bb9fb636864776aff75b3
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2025-02-13 02:11:44 -0800

    mounting multiple routers

--- app.mjs
+++ app.mjs
@@ -1,5 +1,7 @@
 import express from 'express';
 import fs from 'fs';
+import morgan from 'morgan';
+
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
@@ -7,9 +9,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-
+app.use(express.json());
+app.use(morgan('dev'));
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8'));
 
+// Tours handlers
 const getAllTours = (req, res) => {
 	res.status(200).json({
 		status: 'success',
@@ -91,9 +95,71 @@ const deleteTour = (req, res) => {
 	});
 };
 
-app.route("/api/v1/tours").get(getAllTours).post(createTour);
+// Users handlers
+const getAllUsers = (req, res) => {
+	res.status(500).json({
+		status: 'error',
+		message: 'This route is not yet defined'
+	});
+};
 
-app.route("/api/v1/tours/:id").get(getTour).patch(updateTour).delete(deleteTour);
+const getUser = (req, res) => {
+	res.status(500).json({
+		status: 'error',
+		message: 'This route is not yet defined'
+	});
+};
+
+const createUser = (req, res) => {
+	res.status(500).json({
+		status: 'error',
+		message: 'This route is not yet defined'
+	});
+};
+
+const updateUser = (req, res) => {
+	res.status(500).json({
+		status: 'error',
+		message: 'This route is not yet defined'
+	});
+};
+
+const deleteUser = (req, res) => {
+	res.status(500).json({
+		status: 'error',
+		message: 'This route is not yet defined'
+	});
+};
+
+// Routes
+
+const tourRouter = express.Router();
+const userRouter = express.Router();
+
+tourRouter
+	.route('/')
+	.get(getAllTours)
+	.post(createTour);
+
+tourRouter
+  .route('/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+userRouter
+	.route('/')
+	.get(getAllUsers)
+	.post(createUser);
+
+userRouter
+  .route('/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 
 const PORT = 8000;
