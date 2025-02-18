97877992123f77f4ce12e5adbcdd81f9b5a055a9
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2025-02-18 00:27:59 -0800

    endpoints

--- app.mjs
+++ app.mjs
@@ -1,6 +1,8 @@
 import express from 'express';
-import fs from 'fs';
 import morgan from 'morgan';
+
+import tourRouter from './routes/tourRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
@@ -9,160 +11,30 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
+
+// 1) MIDDLEWARES
+
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+	app.use(express.json());
+	app.use(express.static(`${__dirname}/public`));
+}
+
 app.use(express.json());
-app.use(morgan('dev'));
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8'));
+app.use(express.static(`${__dirname}/public`));
 
-// Tours handlers
-const getAllTours = (req, res) => {
-	res.status(200).json({
-		status: 'success',
-		results: tours.length,
-		data: {
-			tours
-		}
-	});
-};
+app.use((req, res, next) => {
+	console.log('Hello from the middleware 👋');
+	next();
+});
 
-const getTour = (req, res) => {
-	const id = Number(req.params.id);
-	const tour = tours.find(el => el.id === id);
+app.use((req, res, next) => {
+	req.requestTime = new Date().toISOString();
+	next();
+});
 
-	if (!tour) {
-		return res.status(404).json({
-			status: 'fail',
-			message: 'Invalid ID'
-		});
-	}
-
-	res.status(200).json({
-		status: 'success',
-		data: {
-			tour
-		}
-	});
-};
-
-const createTour = (req, res) => {
-	const newId = tours[tours.length - 1].id + 1;
-	const newTour = Object.assign({ id: newId }, req.body);
-
-	tours.push(newTour);
-
-	fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
-		res.status(201).json({
-			status: 'success',
-			data: {
-				tour: newTour
-			}
-		});
-	});
-};
-
-const updateTour = (req, res) => {
-	const id = Number(req.params.id);
-	const tour = tours.find(el => el.id === id);
-
-	if (!tour) {
-		return res.status(404).json({
-			status: 'fail',
-			message: 'Invalid ID'
-		});
-	}
-
-	res.status(200).json({
-		status: 'success',
-		data: {
-			tour: '<Updated tour here...>'
-		}
-	});
-};
-
-const deleteTour = (req, res) => {
-	const id = Number(req.params.id);
-	const tour = tours.find(el => el.id === id);
-
-	if (!tour) {
-		return res.status(404).json({
-			status: 'fail',
-			message: 'Invalid ID'
-		});
-	}
-
-	res.status(204).json({
-		status: 'success',
-		data: null
-	});
-};
-
-// Users handlers
-const getAllUsers = (req, res) => {
-	res.status(500).json({
-		status: 'error',
-		message: 'This route is not yet defined'
-	});
-};
-
-const getUser = (req, res) => {
-	res.status(500).json({
-		status: 'error',
-		message: 'This route is not yet defined'
-	});
-};
-
-const createUser = (req, res) => {
-	res.status(500).json({
-		status: 'error',
-		message: 'This route is not yet defined'
-	});
-};
-
-const updateUser = (req, res) => {
-	res.status(500).json({
-		status: 'error',
-		message: 'This route is not yet defined'
-	});
-};
-
-const deleteUser = (req, res) => {
-	res.status(500).json({
-		status: 'error',
-		message: 'This route is not yet defined'
-	});
-};
-
-// Routes
-
-const tourRouter = express.Router();
-const userRouter = express.Router();
-
-tourRouter
-	.route('/')
-	.get(getAllTours)
-	.post(createTour);
-
-tourRouter
-  .route('/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
-
-userRouter
-	.route('/')
-	.get(getAllUsers)
-	.post(createUser);
-
-userRouter
-  .route('/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
-
+// 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-
-const PORT = 8000;
-app.listen(PORT, () => {
-	console.log('Server is running on port 8000');
-});
+export default app;
