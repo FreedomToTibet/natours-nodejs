d30eea1ee3e762a5ee7f1c8b6273dbbfca984728
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2025-02-18 19:50:33 -0800

    adds mongodb and mongoose

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+import mongoose from 'mongoose';
+
 import dotenv from 'dotenv';
 dotenv.config({ path: './config.env' });
 
 import app from './app.mjs';
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB)
+  .then(() => {
+    console.log('DB connection successful!');
+  })
+  .catch((err) => {
+    console.error('DB connection error:', err);
+  });
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
