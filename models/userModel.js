0b47707b9228bd1c6d7dec9092abf668c191c599
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2025-03-05 02:04:33 -0800

    adds userModel.js

---
+++ models/userModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import validator from 'validator';
+
+const userSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: [true, 'Please tell us your name!']
+	},
+	email: {
+		type: String,
+		required: [true, 'Please provide your email'],
+		unique: true,
+		lowercase: true,
+		validate: [validator.isEmail, 'Please provide a valid email']
+	},
+	photo: String,
+	password: {
+		type: String,
+		required: [true, 'Please provide a password'],
+		minlength: 8,
+		select: false
+	},
+	passwordConfirm: {
+		type: String,
+		required: [true, 'Please confirm your password'],
+		validate: {
+			// only works for CREATE and SAVE!!!
+			validator: function(el) {
+				return el === this.password;
+			},
+			message: 'Passwords are not the same!'
+		}
+	},
+});
+
+export const User = mongoose.model('User', userSchema);
