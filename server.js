a8e796ba17be622352677938a903e5042f47f7ba
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2025-03-05 01:36:57 -0800

    adds error handlers

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import app from './app.mjs';
+
+process.on('uncaughtException', (err) => {
+	console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+	console.error(err.name, err.message);
+	process.exit(1);
+});
 
 dotenv.config({ path: './config.env' });
 
-import app from './app.mjs';
-
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
-mongoose.connect(DB)
+mongoose.connect(DB, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
+	useUnifiedTopology: true
+})
   .then(() => {
     console.log('DB connection successful!');
   })
@@ -16,6 +26,14 @@ mongoose.connect(DB)
   });
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
 	console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+	console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+	console.error(err.name, err.message);
+	server.close(() => {
+		process.exit(1);
+	});
 });
