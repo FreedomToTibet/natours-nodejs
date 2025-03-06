5cba31e50316862cccec9c33aec8a98b229141e2
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2025-03-06 01:43:39 -0800

    creates user and password hash

--- server.js
+++ server.js
@@ -12,12 +12,7 @@ dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
-mongoose.connect(DB, {
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useFindAndModify: false,
-	useUnifiedTopology: true
-})
+mongoose.connect(DB)
   .then(() => {
     console.log('DB connection successful!');
   })
