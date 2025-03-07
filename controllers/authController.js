51be3beff4dcfce4bce3bcb9b38a12b3b3231c97
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2025-03-07 03:14:51 -0800

    adds login user to the database

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,17 +2,19 @@ import jwt from "jsonwebtoken";
 
 import { User } from "../models/userModel.js";
 import catchAsync from "../utils/catchAsync.js";
+import AppError from "../utils/appError.js";
 
 const signToken = (id) => {
 	return jwt.sign({ id }, process.env.JWT_SECRET, {
-		expiresIn: process.env.JWT_EXPIRES_IN,
+		expiresIn: process.env.JWT_EXPIRES_IN || '90d',
 	});
 };
 
 const createSendToken = (user, statusCode, res) => {
 	const token = signToken(user._id);
+	const cookieExpiresIn = parseInt(process.env.JWT_COOKIE_EXPIRES_IN) || 90; 
 	const cookieOptions = {
-		expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+		expires: new Date(Date.now() + cookieExpiresIn * 24 * 60 * 60 * 1000),
 		httpOnly: true,
 	};
 	if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
@@ -40,4 +42,22 @@ export const signup = catchAsync(async (req, res) => {
 	});
 
 	createSendToken(newUser, 201, res);
+});
+
+export const login = catchAsync(async (req, res, next) => {
+	const { email, password } = req.body;
+
+	// if email and password exist
+	if (!email || !password) {
+		return next(new AppError("Please provide email and password!", 400));
+	}
+	// if user exists && password is correct
+	const user = await User.findOne({ email }).select("+password");
+
+	if (!user || !(await user.correctPassword(password, user.password))) {
+		return next(new AppError("Incorrect email or password", 401));
+	}
+
+	// if everything is ok, send token to client
+	createSendToken(user, 200, res);
 });
--- models/userModel.js
+++ models/userModel.js
@@ -1,49 +1,57 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 import validator from 'validator';
 import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: [true, 'Please tell us your name!']
-	},
-	email: {
-		type: String,
-		required: [true, 'Please provide your email'],
-		unique: true,
-		lowercase: true,
-		validate: [validator.isEmail, 'Please provide a valid email']
-	},
-	photo: String,
-	password: {
-		type: String,
-		required: [true, 'Please provide a password'],
-		minlength: 8,
-		select: false
-	},
-	passwordConfirm: {
-		type: String,
-		required: [true, 'Please confirm your password'],
-		validate: {
-			// only works for CREATE and SAVE!!!
-			validator: function(el) {
-				return el === this.password;
-			},
-			message: 'Passwords are not the same!'
-		}
-	},
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // only works for CREATE and SAVE!!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!',
+    },
+    select: false,
+  },
 });
 
-userSchema.pre('save', async function(next) {
-	// if password wasn't modified
-	if (!this.isModified('password')) return next();
+userSchema.pre('save', async function (next) {
+  // if password wasn't modified
+  if (!this.isModified('password')) return next();
 
-	// if password was modified
-	this.password = await bcrypt.hash(this.password, 12);
+  // if password was modified
+  this.password = await bcrypt.hash(this.password, 12);
 
-	// delete passwordConfirm field
-	this.passwordConfirm = undefined;
-	next();
+  // delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
 });
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 export const User = mongoose.model('User', userSchema);
