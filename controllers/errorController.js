d334387c5620b9daa9f479fdca3728ccb8599c04
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2025-03-08 01:12:44 -0800

    protecting tour routes

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -12,6 +12,12 @@ const handleValidationErrorDB = err => {
   return new AppError(message, 400);
 };
 
+const handleJWTError = () =>
+	new AppError('Invalid token. Please log in again!', 401);
+
+const handleJWTExpiredError = () =>
+	new AppError('Your token has expired! Please log in again.', 401);
+
 const handleDuplicateFieldsDB = err => {
   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
   console.log(value);
@@ -64,6 +70,8 @@ export default (err, req, res, next) => {
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (error.name === 'ValidationError')
       error = handleValidationErrorDB(error);
+		if (error.name === 'JsonWebTokenError') error = handleJWTError();
+		if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
 
     sendErrorProd(error, res);
   }
