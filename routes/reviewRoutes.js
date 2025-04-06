c73ded5c5116f930db77dc19e8336c279cfa4d58
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2025-04-06 00:36:41 -0700

    updates routes

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -3,19 +3,27 @@ import * as reviewController from '../controllers/reviewController.js';
 import * as authController from '../controllers/authController.js';
 
 const router = express.Router({ mergeParams: true });
+router.use(authController.protect);
 
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-		authController.protect,
-		authController.restrictTo('user'),
-		reviewController.setTourUserIds,
-		reviewController.createReview);
+    authController.restrictTo('user'),
+    reviewController.setTourUserIds,
+    reviewController.createReview
+  );
 
-router.route('/:id')
-	.get(reviewController.getReview)
-	.patch(reviewController.updateReview)
-	.delete(reviewController.deleteReview);
+router
+  .route('/:id')
+  .get(reviewController.getReview)
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview
+  )
+  .delete(
+    authController.restrictTo('user', 'admin'),
+    reviewController.deleteReview
+  );
 
 export default router;
