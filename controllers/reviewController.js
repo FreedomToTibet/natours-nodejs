a59daab15295ae603dcc6e29bd2b953fb3204332
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2025-04-05 19:57:09 -0700

    adds handler factory function Read

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,23 +1,14 @@
 import Review from "../models/reviewModel.js";
-import catchAsync from "../utils/catchAsync.js";
 
-import { deleteOne, updateOne, createOne } from './handlerFactory.js';
+import { deleteOne, updateOne, createOne, getOne, getAll } from './handlerFactory.js';
 
-export const getAllReviews = catchAsync(async (req, res, next) => {
-	// Allow for nested GET reviews on tour (hack)
-	let filter = {};
-	if (req.params.tourId) filter = { tour: req.params.tourId };
 
-	const reviews = await Review.find(filter);
+export const createReview = createOne(Review);
+export const updateReview = updateOne(Review);
+export const deleteReview = deleteOne(Review);
 
-	res.status(200).json({
-		status: "success",
-		results: reviews.length,
-		data: {
-			reviews,
-		},
-	});
-});
+export const getReview = getOne(Review);
+export const getAllReviews = getAll(Review);
 
 export const setTourUserIds = (req, res, next) => {
   // Allow nested routes
@@ -25,7 +16,3 @@ export const setTourUserIds = (req, res, next) => {
   if (!req.body.user) req.body.user = req.user.id;
   next();
 };
-
-export const createReview = createOne(Review);
-export const updateReview = updateOne(Review);
-export const deleteReview = deleteOne(Review);
