ad045dd64f5fb304cf08e6f384acc1d8b0d8e653
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2025-04-13 01:22:35 -0700

    adds tour overview page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,15 @@
-export const getOverview = (req, res) => {
+import Tour from "../models/tourModel.js";
+import catchAsync from "../utils/catchAsync.js";
+
+export const getOverview = catchAsync (async (req, res, next) => {
+	const tours = await Tour.find();
+
 	res.status(200).render('overview', {
 		title: 'All Tours',
+		tours,
 	});
-};
+	next();
+});
 
 export const getTour = (req, res) => {
 	res.status(200).render('tour', {
