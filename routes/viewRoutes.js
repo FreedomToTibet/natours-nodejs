64605161773e848ebb4fb6dacfb9e4dc677329d2
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2025-04-15 01:40:04 -0700

    adds login page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -7,4 +7,9 @@ router.get('/', viewController.getOverview);
 
 router.get('/tour/:slug', viewController.getTour);
 
+router.get('/login', viewController.getLoginForm);
+// router.get('/me', viewController.getAccount);
+// router.get('/logout', viewController.logoutUser);
+// router.get('/my-tours', viewController.getMyTours);
+// router.get('/my-reviews', viewController.getMyReviews);
 export default router;
