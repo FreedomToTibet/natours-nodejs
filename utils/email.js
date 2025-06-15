acfea691339c5d84457753773592b6b3cd614373
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2025-06-15 02:08:09 -0700

    adds greeting email option before brevo

--- utils/email.js
+++ utils/email.js
@@ -1,28 +1,103 @@
 import nodemailer from 'nodemailer';
+import pug from 'pug';
+import { htmlToText } from 'html-to-text';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
 
-const sendEmail = async options => {
-	// 1) Create a transporter
-	const transporter = nodemailer.createTransport({
-		host: process.env.EMAIL_HOST,
-		port: process.env.EMAIL_PORT,
-		auth: {
-			user: process.env.EMAIL_USERNAME,
-			pass: process.env.EMAIL_PASSWORD
-		}
-	});
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-	// 2) Define the email options
-	const mailOptions = {
-		from: 'Andrew <hello@andrew.mail>',
-		to: options.email,
-		subject: options.subject,
-		text: options.message
-		// html:
-	};
+export default class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Andrew <${process.env.EMAIL_FROM}>`;
+  }
 
-	// 3) Actually send the email
-	await transporter.sendMail(mailOptions);
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // SendGrid
+      return nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
+        },
+      });
+    }
+
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  async send(template, subject) {
+    const html = pug.renderFile(
+      path.join(__dirname, '..', 'views', 'emails', `${template}.pug`),
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      }
+    );
+
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText(html),
+    };
+
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    try {
+      await this.send('welcome', 'Welcome to the Natours Family!');
+    } catch (err) {
+      console.error('Email error:', err);
+      // Continue without sending email
+    }
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (valid for only 10 minutes)'
+    );
+  }
 }
 
+// const sendEmail = async options => {
+// 	// 1) Create a transporter
+// 	const transporter = nodemailer.createTransport({
+// 		host: process.env.EMAIL_HOST,
+// 		port: process.env.EMAIL_PORT,
+// 		auth: {
+// 			user: process.env.EMAIL_USERNAME,
+// 			pass: process.env.EMAIL_PASSWORD
+// 		}
+// 	});
 
-export default sendEmail;
+// 	// 2) Define the email options
+// 	const mailOptions = {
+// 		from: 'Andrew <hello@andrew.mail>',
+// 		to: options.email,
+// 		subject: options.subject,
+// 		text: options.message
+// 		// html:
+// 	};
+
+// 	// 3) Actually send the email
+// 	await transporter.sendMail(mailOptions);
+// }
+
+// export default sendEmail;
