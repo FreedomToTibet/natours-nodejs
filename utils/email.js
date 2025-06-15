a7e4af1edccf97951dbafb6f1a68a84dba85ddc4
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2025-06-15 03:28:21 -0700

    adds Brevo service for greeting sign up email

--- utils/email.js
+++ utils/email.js
@@ -18,12 +18,14 @@ export default class Email {
 
   newTransport() {
     if (process.env.NODE_ENV === 'production') {
-      // SendGrid
+      // Brevo
+			console.log('production');
       return nodemailer.createTransport({
-        service: 'SendGrid',
+        host: process.env.BREVO_HOST,
+				port: process.env.BREVO_PORT,
         auth: {
-          user: process.env.SENDGRID_USERNAME,
-          pass: process.env.SENDGRID_PASSWORD,
+          user: process.env.BREVO_LOGIN,
+          pass: process.env.BREVO_PASSWORD,
         },
       });
     }
@@ -75,29 +77,3 @@ export default class Email {
     );
   }
 }
-
-// const sendEmail = async options => {
-// 	// 1) Create a transporter
-// 	const transporter = nodemailer.createTransport({
-// 		host: process.env.EMAIL_HOST,
-// 		port: process.env.EMAIL_PORT,
-// 		auth: {
-// 			user: process.env.EMAIL_USERNAME,
-// 			pass: process.env.EMAIL_PASSWORD
-// 		}
-// 	});
-
-// 	// 2) Define the email options
-// 	const mailOptions = {
-// 		from: 'Andrew <hello@andrew.mail>',
-// 		to: options.email,
-// 		subject: options.subject,
-// 		text: options.message
-// 		// html:
-// 	};
-
-// 	// 3) Actually send the email
-// 	await transporter.sendMail(mailOptions);
-// }
-
-// export default sendEmail;
